9c932a36d10a01dd43400a09c58ef43d498822e7
KingMaty <147078761+KingMaty@users.noreply.github.com>
2024-03-04 02:18:56 +0100

    Add files via upload

---
+++ Finální verze/database.py
@@ -0,0 +1,44 @@
+import mysql.connector
+from db_config import DB_CONFIG
+
+def connect():
+    try:
+        conn = mysql.connector.connect(**DB_CONFIG)
+        return conn
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return None
+    
+def insert_data(conn, query, data):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(query, data)
+        conn.commit()
+        cursor.close()
+        return True
+    except mysql.connector.Error as error:
+        print("Error inserting data:", error)
+        conn.rollback()
+        return False
+    else :
+        return False
+    finally:
+        return True
+
+def delete_data(conn, query, data):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(query, data)
+        conn.commit()
+        cursor.close()
+        return True
+    except mysql.connector.Error as error:
+        print("Error deleting data:", error)
+        conn.rollback()
+        return False
+    else :
+        return False
+    finally:
+        return True
+
+    
